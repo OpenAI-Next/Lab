fce6749cddaee7ac743d2bce4bc0b640f99cfd2f
Kadxy <2230318258@qq.com>
2024-09-11 10:56:56 +0800

    feat(pricing):add model icon

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
 
         return config;
     },
-    transpilePackages: ['@lobehub/icons'],
+    // transpilePackages: [],
     output: mode,
     reactStrictMode: false,
     images: {
